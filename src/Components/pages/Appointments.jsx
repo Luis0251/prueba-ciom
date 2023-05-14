3bd52dd871ac4b07344aa877f9b4d0f0644becdb
Luis0251 <luis0251@hotmail.com>
2023-05-14 14:20:49 -0500

    feature:create component Appoitments

---
+++ src/Components/pages/Appointments.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TaskList } from '../TaskList'
+
+export const Appoitments = () => {
+  return (
+    <section className='container'>
+        <div className='row justify-content-between gap-4'>
+            <TaskList />
+        </div>
+    </section>
+  )
+}
