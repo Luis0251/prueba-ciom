50e1ab41905326d5db1183246a74194f86944ee7
Luis0251 <luis0251@hotmail.com>
2023-05-14 13:06:26 -0500

    feature:create component Form task

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CustomForm } from './CustomForm'
 
 export const Header = ({company}) => {
     const {name} = company
@@ -6,6 +7,9 @@ export const Header = ({company}) => {
     <header className='py-3 text-center'>
         <div className='d-flex flex-column gap-1 container'>
             <h2 className='text-warning m-0'>{name}</h2>
+            <main className="py-3">
+        <CustomForm />
+      </main>
         </div>
     </header>
   )
