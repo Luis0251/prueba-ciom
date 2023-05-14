a115cf8845adc95487a1044bf6b263d54ae13adc
Luis0251 <luis0251@hotmail.com>
2023-05-14 14:04:04 -0500

    feature:added component TaskList

---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TaskList = () => {
+  return (
+    <section className='d-flex flex-column gap-3 col-md-6'>
+        <h3 className='text-light text-center m-0'>Task List</h3>
+    </section>
+  )
+}
